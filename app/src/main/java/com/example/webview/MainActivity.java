ecbc0e114ff71fe5028108a0cc82d76c6d9b0cf4
dinki agrawal-15 <dinkiagrawal77@gmail.com>
2021-04-25 12:38:44 +0000

    initial commit

---
+++ app/src/main/java/com/example/webview/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.webview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView webView=findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://www.google.com");
+        //webView.loadData("<html></html>");
+    }
+}
